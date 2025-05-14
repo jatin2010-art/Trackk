cee22429b46bc0ca6fe8c68ccfe2bda1e71fd7f8
bhavy1204 <bhavysarwa51@gmail.com>
2025-05-14 13:21:13 +0530

    feat: complete taskDetail functions and updated task view

--- index.js
+++ index.js
@@ -2,54 +2,8 @@
 
 import inquirer from 'inquirer';
 
-// const stack = [];
-// const choiceSTack = [];
-
-// const main = async () => {
-//     const answer = await inquirer.prompt([{
-//         type: "input",
-//         name: "temp",
-//         message: " This is message",
-//         prefix: ""
-//     }]);
-//     stack.push(answer.temp);
-// }
-
-// const choice = async () => {
-//     const choice = await inquirer.prompt([{
-//         type: "list",
-//         name: "choice",
-//         message: "Kya chaaiye inme se tuje..? ",
-//         prefix: "",
-//         choices: [
-//             "Choice 1",
-//             "choice2 ",
-//             "choice 3",
-//             "choice 4"
-//         ]
-//     }])
-//     if (choice.choice === "Choice 1") {
-//         await main();
-//     }
-//     choiceSTack.push(choice.choice);
-// }
-// // main();
-// let i = 3;
-// const multiLop = async () => {
-//     while (i--) {
-//         await main();
-//     }
-// }
-
-// (async ()=>{
-//     await choice();
-//     console.log(choiceSTack);
-//     console.log(stack);
-// })();
-
-
 const toDos = [];
-const completed = [];
+const completedTasks = [];
 
 const addTask = async () => {
     const response = await inquirer.prompt([{
@@ -62,37 +16,66 @@ const addTask = async () => {
     console.log(`Task added : ${response.task}`);
 }
 
+const showTaskDetails = async (idx) => {
+    const task = toDos[idx];
+
+    console.log(`TASK DETAILS \nTASK : ${task.task}`);
+
+    const { action } = await inquirer.prompt([{
+        type: "list",
+        name: "action",
+        message: "--> ",
+        choices: [
+            "1. Complete this task",
+            "2. Delete this task",
+            "3. Edit",
+            "4. go back",
+        ]
+    }])
+    if (action === "1. Complete this task") {
+        const compTask = { ...task, done: true };
+        completedTasks.push(compTask);
+        toDos.splice(idx, 1);
+        console.log("Task completed !! congrats");
+    } else if (action === "2. Delete this task") {
+        toDos.splice(idx, 1);
+        console.log("task deleted successfully");
+    } else if (action === "3. Edit") {
+        console.log("CURRENTLY NOT AVAILABLE...");
+    } else if (action === "4. go back") {
+        return
+    } else
+        return
+}
+
 const showTask = async () => {
-    if (toDos.length === 0){
+    if (toDos.length === 0) {
         console.log("No tasks to show");
         return;
     }
-    const taskChoice = toDos.map((item,idx)=>({
-        name:`[${idx+1}] ${item.task}`,
+    const taskChoice = toDos.map((item, idx) => ({
+        name: `[${idx + 1}] ${item.task}`,
         value: idx,
     }));
 
-    taskChoice.push({name:"GO BACK", value:-1});
+    taskChoice.push({ name: "GO BACK", value: -1 });
 
     const selected = await inquirer.prompt([{
-        type:'list',
-        name:"selected",
-        message:"YOURS TODAY's TASKS",
-        choices:taskChoice,
+        type: 'list',
+        name: "selected",
+        message: "YOURS TODAY's TASKS",
+        choices: taskChoice,
     }]);
 
-    if(selected===-1)
+    if (selected.selected === -1)
         return;
-    console.log(`YOU SELECTED ${selected}`);
+    await showTaskDetails(selected.selected);
 }
 
-const removetask = async () => {
-    if (toDos.length === 0)
-        console.log("No task to delete");
-    else {
-        let deletedtask = toDos.pop();
-        console.log(`Task deleted : ${deletedtask}`);
-    }
+const showCompletedTask = async () => {
+    completedTasks.forEach((element, idx) => {
+        console.log(`${idx + 1}. ${element.task}`);
+    });
 }
 
 const menu = async () => {
@@ -104,17 +87,17 @@ const menu = async () => {
             message: "\n\t----- WELCOME TO THE APP ----- \t\n",
             choices: [
                 "1. Add task",
-                "2. Remove task",
-                "3. View all task",
+                "2. View tasks",
+                "3. View completed tasks",
                 "4.exit"
             ]
         }]);
         if (response.choice === "1. Add task")
             await addTask();
-        else if (response.choice === "2. Remove task")
-            removetask();
-        else if (response.choice === "3. View all task")
+        else if (response.choice === "2. View tasks")
             await showTask();
+        else if (response.choice === "3. View completed tasks")
+            await showCompletedTask();
         else if (response.choice === "4.exit") {
             exit = true;
             console.log("\nBye bye !! ");
